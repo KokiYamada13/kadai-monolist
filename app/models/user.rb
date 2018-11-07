9b91e58af222f09be9819a398265ab398606e509
koki yamada <lenny.srv.826@gmail.com>
2018-11-07 15:52:58 +0000

    want

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
-  has_secure_password :ownerships
+  has_secure_password 
+  
+  has_many :ownerships
   has_many :items, through: :ownerships
   has_many :wants
   has_many :want_items, through: :wants, class_name: 'Item', source: :item
